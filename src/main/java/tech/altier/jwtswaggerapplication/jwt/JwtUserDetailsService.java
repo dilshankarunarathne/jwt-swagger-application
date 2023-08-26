f6c240bd6c045065800a448fd7152991f1689015
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-26 09:47:58 +0530

    Update JwtUserDetailsService.java

--- src/main/java/tech/altier/jwtswaggerapplication/jwt/JwtUserDetailsService.java
+++ src/main/java/tech/altier/jwtswaggerapplication/jwt/JwtUserDetailsService.java
@@ -1,4 +1,27 @@
 package tech.altier.jwtswaggerapplication.jwt;
 
-public class JwtUserDetailsService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import tech.altier.repository.UserRepository;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Retrieve the user from your database (replace with your logic)
+        YourUserEntity userEntity = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        // Map your user entity to UserDetails
+        return new User(userEntity.getUsername(), userEntity.getPassword(), /* add authorities/roles */);
+    }
 }
+
