997706534ff05afa59581e4b809cce31056a3a59
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-26 09:57:40 +0530

    Update User.java

--- src/main/java/tech/altier/jwtswaggerapplication/jpa/User.java
+++ src/main/java/tech/altier/jwtswaggerapplication/jpa/User.java
@@ -13,5 +13,28 @@ public class User {
     private Long id;
     private String username;
     private String password;
-    // Other user properties, getters, and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
