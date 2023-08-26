a36863061d610d2440446e9606d3cb21ec5b0f60
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-26 10:26:20 +0530

    Update JwtSwaggerApplication.java

--- src/main/java/tech/altier/jwtswaggerapplication/JwtSwaggerApplication.java
+++ src/main/java/tech/altier/jwtswaggerapplication/JwtSwaggerApplication.java
@@ -3,9 +3,11 @@ package tech.altier.jwtswaggerapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = "tech.altier.jwtswaggerapplication.jpa")
+@EnableJpaRepositories(basePackages = "tech.altier.jwtswaggerapplication.repository")
 public class JwtSwaggerApplication {
 
     public static void main(String[] args) {
