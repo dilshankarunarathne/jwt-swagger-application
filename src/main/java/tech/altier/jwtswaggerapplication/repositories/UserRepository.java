e57e75ceb2fdac1355b8f866762699c95ea0c177
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-26 09:26:16 +0530

    Update UserRepository.java

--- src/main/java/tech/altier/jwtswaggerapplication/repositories/UserRepository.java
+++ src/main/java/tech/altier/jwtswaggerapplication/repositories/UserRepository.java
@@ -1,4 +1,8 @@
 package tech.altier.jwtswaggerapplication.repositories;
 
-public class UserRepository {
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
