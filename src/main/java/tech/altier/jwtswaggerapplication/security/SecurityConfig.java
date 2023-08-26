086b93a267465b215397ecd028cd0bcc8d5cf77c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-26 09:39:11 +0530

    Update SecurityConfig.java

--- src/main/java/tech/altier/jwtswaggerapplication/security/SecurityConfig.java
+++ src/main/java/tech/altier/jwtswaggerapplication/security/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
