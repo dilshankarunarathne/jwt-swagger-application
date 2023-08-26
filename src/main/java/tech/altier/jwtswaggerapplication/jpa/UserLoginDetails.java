c634f76e6ea5b667099cd0ee8feab46bea871281
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-26 09:25:35 +0530

    Update UserLoginDetails.java

--- src/main/java/tech/altier/jwtswaggerapplication/jpa/UserLoginDetails.java
+++ src/main/java/tech/altier/jwtswaggerapplication/jpa/UserLoginDetails.java
@@ -1,4 +1,13 @@
 package tech.altier.jwtswaggerapplication.jpa;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class UserLoginDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private String loginTime;
+    // Other login details, getters, and setters
 }
