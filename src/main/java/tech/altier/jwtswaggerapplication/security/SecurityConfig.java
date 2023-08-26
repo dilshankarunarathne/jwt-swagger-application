01cd97b6f1a546cb56d4d4054782d01e5eb9274d
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-26 09:38:55 +0530

    Update SecurityConfig.java

--- src/main/java/tech/altier/jwtswaggerapplication/security/SecurityConfig.java
+++ src/main/java/tech/altier/jwtswaggerapplication/security/SecurityConfig.java
@@ -1,27 +1,4 @@
 package tech.altier.jwtswaggerapplication.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-    @Autowired
-    private JwtRequestFilter jwtRequestFilter;
-
-    // Configure authentication and authorization here
-
-    @Override
-    public void configure(HttpSecurity httpSecurity) throws Exception {
-        // Configure security settings, such as authorization rules
-        // ...
-
-        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-    }
-}
 
