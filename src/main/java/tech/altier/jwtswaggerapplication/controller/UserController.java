d82b9704fa25ce6acf9595968b6b37ce4a81b474
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-26 11:31:40 +0530

    Update UserController.java

--- src/main/java/tech/altier/jwtswaggerapplication/controller/UserController.java
+++ src/main/java/tech/altier/jwtswaggerapplication/controller/UserController.java
@@ -1,4 +1,14 @@
 package tech.altier.jwtswaggerapplication.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+    @GetMapping("/all")
+    public String allAccess() {
+        return "Public Content.";
+    }
 }
