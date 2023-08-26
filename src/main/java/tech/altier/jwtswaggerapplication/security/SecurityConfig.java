788b2af2f3d1d135f612eab4ed66ceb741d958db
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-26 09:39:03 +0530

    Update SecurityConfig.java

--- src/main/java/tech/altier/jwtswaggerapplication/security/SecurityConfig.java
+++ src/main/java/tech/altier/jwtswaggerapplication/security/SecurityConfig.java
@@ -1,4 +1,26 @@
 package tech.altier.jwtswaggerapplication.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers("/public/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults());
+        return http.build();
+    }
+}
 
 
