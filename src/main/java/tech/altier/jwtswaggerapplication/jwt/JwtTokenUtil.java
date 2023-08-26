897f8d58c13bd1ccd9cdae850f2350d48dc946ea
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-26 09:47:17 +0530

    Update JwtTokenUtil.java

--- src/main/java/tech/altier/jwtswaggerapplication/jwt/JwtTokenUtil.java
+++ src/main/java/tech/altier/jwtswaggerapplication/jwt/JwtTokenUtil.java
@@ -1,4 +1,4 @@
-package tech.altier.jwtswaggerapplication.security;
+package tech.altier.jwtswaggerapplication.jwt;
 
 import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
