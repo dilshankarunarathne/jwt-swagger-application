f7a89656b637ccbfc836d744895e6db9aea235a7
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-26 09:48:06 +0530

    Create UserRepository.java

---
+++ src/main/java/tech/altier/jwtswaggerapplication/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package tech.altier.jwtswaggerapplication.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tech.altier.jwtswaggerapplication.jpa.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
