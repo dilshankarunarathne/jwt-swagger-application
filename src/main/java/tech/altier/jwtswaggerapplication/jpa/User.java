c55f0412a83e8bee9ef1ecb686c36959bca56e1f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-26 09:24:52 +0530

    Update User.java

--- src/main/java/tech/altier/jwtswaggerapplication/jpa/User.java
+++ src/main/java/tech/altier/jwtswaggerapplication/jpa/User.java
@@ -1,4 +1,13 @@
 package tech.altier.jwtswaggerapplication.jpa;
 
+import jakarta.persistence.GenerationType;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    // Other user properties, getters, and setters
 }
