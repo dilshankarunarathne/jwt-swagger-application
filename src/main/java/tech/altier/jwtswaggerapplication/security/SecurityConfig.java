1794b1a9f9d159c630f2bced72db3dd5dfbeda50
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-26 09:28:11 +0530

    Update SecurityConfig.java

--- src/main/java/tech/altier/jwtswaggerapplication/security/SecurityConfig.java
+++ src/main/java/tech/altier/jwtswaggerapplication/security/SecurityConfig.java
@@ -1,7 +1,10 @@
 package tech.altier.jwtswaggerapplication.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
