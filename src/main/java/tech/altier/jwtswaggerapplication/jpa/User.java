e20e5257bb75023ef2862d6eda3a05ab6224cf68
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-26 10:23:09 +0530

    Update User.java

--- src/main/java/tech/altier/jwtswaggerapplication/jpa/User.java
+++ src/main/java/tech/altier/jwtswaggerapplication/jpa/User.java
@@ -1,9 +1,8 @@
 package tech.altier.jwtswaggerapplication.jpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
