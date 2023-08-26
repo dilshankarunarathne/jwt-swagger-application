13f8842764f35fec01e7ef58c38a229acdecb65f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-26 09:22:45 +0530

    Create JwtSwaggerApplicationTests.java

---
+++ src/test/java/tech/altier/jwtswaggerapplication/JwtSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.altier.jwtswaggerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
