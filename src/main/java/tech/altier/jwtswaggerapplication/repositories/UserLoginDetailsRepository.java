0729817d2d9dced6f0f221c8d0a047517b2d4269
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-26 09:26:42 +0530

    Update UserLoginDetailsRepository.java

--- src/main/java/tech/altier/jwtswaggerapplication/repositories/UserLoginDetailsRepository.java
+++ src/main/java/tech/altier/jwtswaggerapplication/repositories/UserLoginDetailsRepository.java
@@ -1,7 +1,10 @@
 package tech.altier.jwtswaggerapplication.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tech.altier.jwtswaggerapplication.jpa.UserLoginDetails;
+
+import java.util.List;
 
 @Repository
 public interface UserLoginDetailsRepository extends JpaRepository<UserLoginDetails, Long> {
