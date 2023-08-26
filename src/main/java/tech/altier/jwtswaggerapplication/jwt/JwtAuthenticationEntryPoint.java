34c4a8e3495347bebd73dbb50bbfb3edbb0ae62e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-26 09:59:00 +0530

    Update JwtAuthenticationEntryPoint.java

--- src/main/java/tech/altier/jwtswaggerapplication/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/tech/altier/jwtswaggerapplication/jwt/JwtAuthenticationEntryPoint.java
@@ -1,4 +1,19 @@
 package tech.altier.jwtswaggerapplication.jwt;
 
-public class JwtAuthenticationEntryPoint {
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
 }
+
